1525b4b90a25d8e029834210b631d14bc489c0bb
Enqu1 <114190936+Enqu1@users.noreply.github.com>
2023-09-26 19:26:08 +0100

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,33 @@
+import { useEffect, useState } from "react";
+
+const colors = ["red", "green", "yellow", "blue"];
+
+function App() {
+  const [gameColors, setGameColors] = useState<string[]>([]);
+  const [matchedCards, setMatchedCards] = useState<string[]>([]);
+
+  useEffect(() => {
+    setGameColors(colors.concat(colors).sort(() => Math.random() - 0.5));
+  }, []);
+
+  return (
+    <>
+      {gameColors.map((color, index) => (
+        <div className="card">
+          {matchedCards.includes(color) ? (
+            <div style={{ backgroundColor: color }} key={index}>
+              {color} {index}
+            </div>
+          ) : (
+            <div key={index}>
+              {color} {index}
+            </div>
+          )}
+        </div>
+      ))}
+      ;
+    </>
+  );
+}
+
+export default App;
